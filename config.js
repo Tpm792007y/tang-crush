f72b9865fbf66388d383d12d11ca28bf1cad8fbd
Tpm792007y <86836626+Tpm792007y@users.noreply.github.com>
2021-07-02 21:05:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Thỏ Trắng à!',
+    introTitle: 'ĐỖ HOÀNG ĐỨC à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Đức cứ đi nhầm đường
+    Lạc vào tim Đức lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'ấn vào xem đô ngu của thầy Đức',
+    title: 'Phải chăng Đức đã yêu Phương ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Đức  đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'có cái loz địt cụ ĐỖ HOÀNG ĐỨC <33',
+    btnNo: 'Không, Đức đẹp trAI :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho Phương lời thân ái <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
